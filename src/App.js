47107cd19447e0b31125226681a8f7e6cec9a82e
BwanaQ <thunjawax@gmail.com>
2021-07-26 16:15:22 +0300

    ft add toggle form

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import {useState} from 'react'
 import Header from './components/Header';
 import './App.css';
 import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 
 function App() {
+  const[showAddTask,setShowAddTask]= useState(false)
   const [tasks, setTasks]= useState(
     [
       {
@@ -26,6 +28,12 @@ function App() {
       },
     ]
   )
+  //Add Task
+  const addTask = (task)=>{
+    const id = Math.floor(Math.random()*10000)+1
+    const newTask = {id,...task}
+    setTasks([...tasks,newTask])
+  }
   //Delete Task
   const deleteTask=(id)=>{
     setTasks(tasks.filter((task)=> task.id!==id))
@@ -41,8 +49,12 @@ function App() {
   }
   return (
     <div className="container">
-      <Header/>
-      <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
+      <Header 
+        onAdd = {()=>setShowAddTask(!showAddTask)}
+        showAdd={showAddTask} 
+      />
+      {showAddTask && <AddTask onAdd = {addTask}/>}
+      {tasks.length>0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>:'No Tasks'}
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,12 @@
 import PropTypes from 'prop-types';
 import Button from './Button';
-const Header = ({title}) => {
-  const onClick = ()=>{
-    console.log('click')
-  }
+const Header = ({title, onAdd, showAdd}) => {
+
 
   return (
   <header className='header'>
     <h1>{title}</h1>
-    <Button onClick={onClick} color='green' text='Hello'/>
+    <Button onClick={onAdd} color={showAdd? 'red':'green'} text={showAdd? 'close':'Add'}/>
   </header>);
 };
 Header.defaultProps={
